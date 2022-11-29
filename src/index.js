3e2528b5b8ad48fc9f13b4a757ecb3b1ec630f37
Henrique Castro <henrique.monteiro.castro@outlook.com>
2022-11-29 11:14:39 -0300

    Conceito Node.Js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const { v4: uuidv4 } = require('uuid');
+const { toObject } = require('json-react');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!username) {
+      response.status(400).json({
+        message: 'Username is required'
+      });
+      return;
+  }
+  request.user = user;
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const userExists = users.find(user => user.username === username);
+  if (userExists) {
+    return response.status(400).json({
+      message: 'User already exists',
+    });
+  }
+
+  const user = { 
+    id: uuidv4(), 
+    name, 
+    username, 
+    todos: []
+  }
+  users.push(user);
+  
+  return response.status(201).json(user)
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user} = request;
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(todo);
+  return response.status(201).json(todo);
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Not Found' });
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Not Found' });
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: 'Not Found' });
+}
+
+user.todo.splice(todoIndex, 1)
+return response.status(204).json({ error: 'OK' });
+});
+
+module.exports = app;
